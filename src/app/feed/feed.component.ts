d34da63cd559295d62f761b6b1fac165266a9003
Vladislav Volosnikov <Vladyan18@gmail.com>
2017-12-12 18:22:02 +0300

    prepared for azure5

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -16,6 +16,7 @@ export class FeedComponent implements OnInit{
   @Input() post: Post;
   @Input() profile: any;
   liked: boolean;
+  opened: boolean;
   comments: any[];
 
   constructor(private http: HttpClient, public auth: AuthService) {
